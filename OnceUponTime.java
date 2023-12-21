8b6cc5ec7fa54a55a9c3db72b635b9d5f77261ab
Taiwo Adejare <55572807+jboy457@users.noreply.github.com>
2023-12-21 11:49:17 -0600

    Delete OnceUponTime.java

--- OnceUponTime.java
+++
@@ -1,7 +0,0 @@
-public class OnceUponTime {
-    public static void main(String[] args) {
-        System.out.println("Once");
-        System.out.println("uponA");
-        System.out.println("time");
-    }
-}
